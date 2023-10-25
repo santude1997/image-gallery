8c06b5072dd7211715fa8494575484c043396abf
santude1997 <santude1997@outlook.com>
2023-10-25 17:07:31 +0530

    nav bar added in the project with search box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from './components/Header/Navbar';
 
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         <main className='max-w-6xl mx-auto'>{children}</main>
       </body>
     </html>
