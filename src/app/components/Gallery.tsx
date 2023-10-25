a74053bfa34bb54acee359e5667c9afa17cbe8a7
santude1997 <santude1997@outlook.com>
2023-10-25 16:24:24 +0530

    image showing is completed

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "../lib/fetchImages"
 import type { ImagesResults } from "@/models/Images"
 import ImgContainer from "./ImgContainer"
+import addBlurredDataUrls from "../lib/getBase64";
 
 export default async function Gallery() {
     const url = "https://api.pexels.com/v1/curated";
@@ -10,11 +11,12 @@ export default async function Gallery() {
     if(!images) 
     return <h2 className="m-4 text-2xl font-bold">No images Found</h2>
 
+        const photosWithBlur=await addBlurredDataUrls(images)
     return (
-        <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-            {images.photos.map(photo =>(
-                <ImgContainer photo={photo}/>
-            ))}
-        </section>
-    )
+      <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+        {photosWithBlur.map((photo) => (
+          <ImgContainer key={photo.id} photo={photo} />
+        ))}
+      </section>
+    );
 }
--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,15 +7,14 @@ type Props={
 
 export default function ImgContainer({photo}:Props){
     return (
-      <div
-        key={photo.id}
-        className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
-      >
+      <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
         <Image
           src={photo.src.large}
           alt={photo.alt}
           fill={true}
           sizes="(max-width: 786px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          placeholder="blur"
+          blurDataURL={photo.blurredDataUrl}
           className="object-cover group-hover:opacity-75"
         />
       </div>
